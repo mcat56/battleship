7e6b3783edf68190ca5140e08b95b038da779228
Mary Lang <mcl86@cornell.edu>
2019-08-20 15:54:13 -0600

    Added tests to board_test
    Co-authored-by: Michael Cooper <8922930+CoopTang@users.noreply.github.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -9,6 +9,12 @@ class BoardTest < MiniTest::Test
 
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end
+
+  def test_it_exists
+    assert_instance_of Board, @board
   end
 
   def test_it_has_cells
@@ -18,12 +24,25 @@ class BoardTest < MiniTest::Test
   end
 
   def test_valid_coordinate?
-    assert @board.valid_coordinate?("A1")
-    refute @board.valid_coordinate?("E1")
+    assert_equal true, @board.valid_coordinate?("A1")
+    assert_equal false, @board.valid_coordinate?("E1")
   end
 
   def test_valid_placement?
-    assert_equal cruiser.length, coordinates.length
-    #more code
+    assert_equal true, @board.place(@cruiser,["A1","A2","A3"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "B1"])
   end
+
+  def test_place
+    @board.place(@cruiser,["A1","A2","A3"])
+    assert_equal @cruiser, @board.cells("A1").ship
+    assert_equal @cruiser, @board.cells("A2").ship
+    assert_equal @cruiser, @board.cells("A3").ship
+    @board.place(@submarine,["A1","A2"])
+    assert_equal @cruiser, @board.cells("A1").ship
+    assert_equal @cruiser, @board.cells("A2").ship
+    assert_equal @cruiser, @board.cells("A3").ship
+  end
+
+
 end
