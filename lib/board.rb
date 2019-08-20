90ae67118ad66b1ae01b1e40a84d289252d01185
Mary Lang <mcl86@cornell.edu>
2019-08-20 17:32:27 -0600

    Added to validate coordinates
    Co-authored-by: Michael Cooper <8922930+CoopTang@users.noreply.github.com>

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,31 @@
 class Board
 attr_reader :cells
 
-  def initialize
-    @cells = cells
+  def initialize(length = 4, width = 4)
+    @cells = Hash.new
+    @length = length
+    @width =  width
+    create_board(length, width)
   end
 
-
-  def valid_coordinate?
-    if cell.coordinate.chars.first =~ /^[ABCD/ && cell.coordinate.chars.last =~ /[1234]/
+  def valid_coordinate?(coordinate)
+    if coordinate.chars.first =~ /^[ABCD]/ && coordinate.chars.last =~ /[1234]/
       true
     else
       false
     end
   end
 
+  def create_board(length,width)
+
+  end
+
+
   def valid_placement?(ship,coordinates)
     if ship.length == coordinates.length
-  end
+      row = []
+      column = []
+      coordinates.each do |coordinate|
+        row << coordinate.split("").first
+        column << coordinate.split("").last
+        if  ( [(coordinate.split("").last)..ship.width] == row && [(coorindate.split("").first)]*ship.length == column ) || ( [coordinate.split("").first] == row && ["A"]*ship.length == column )
