657f2b69abd707ec089e50f31aa22abaa98deb1a
Michael Cooper <macoop2363@mgmail.com>
2019-08-20 09:05:24 -0600

    Added functionality to methods
    Co-authored-by: Mary Lang <51488670+mcat56@users.noreply.github.com>

---
+++ test/ship_test.rb
@@ -0,0 +1,48 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_exists
+    assert_instance_of Ship, @cruiser
+  end
+
+  def test_has_name
+    assert_equal "Cruiser", @cruiser.name
+  end
+
+  def test_has_length
+    assert_equal 3, @cruiser.length
+  end
+  
+  def test_health
+    assert_equal 3, @cruiser.health
+    @cruiser.hit
+    assert_equal 2, @cruiser.health
+  end
+
+  def test_sunk?
+    # It should not be sunk until it's hit three times
+    refute @cruiser.sunk?
+    @cruiser.hit
+    refute @cruiser.sunk?
+    @cruiser.hit
+    refute @cruiser.sunk?
+    @cruiser.hit
+
+    # It should be sunk now
+    assert @cruiser.sunk?
+  end
+
+  def test_hit
+    assert_equal 3, @cruiser.health
+    @cruiser.hit
+    assert_equal 2, @cruiser.health
+  end
+
+end
