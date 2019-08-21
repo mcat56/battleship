87a2c5b77a242e1c9a41160ca35cd8abfe9a8d13
Mary Lang <mcl86@cornell.edu>
2019-08-21 11:40:27 -0600

    Update valid placement

--- lib/board.rb
+++ lib/board.rb
@@ -19,13 +19,13 @@ attr_reader :cells, :length, :width
   end
 
   def create_board(length,width)
-
     x = (1..@width).to_a
     y = ("A"..("A".ord+(@length-1)).chr).to_a
-    zips = y.zip(x)
     coordinates = []
-    zips.each do |array|
-      coordinates << array.join
+    y.each do |letter|
+      x.each do |number|
+        coordinates << letter + number.to_s
+      end
     end
     coordinates.each do |coordinate|
       @cells[coordinate] = Cell.new(coordinate)
@@ -34,46 +34,55 @@ attr_reader :cells, :length, :width
   end
 
   def place(ship,coordinates)
-    @cells.values.each do |cell|
-      if cell.empty?
+    valid = []
+    coordinates.each do |coordinate|
+      valid << @cells[coordinate].empty?
+    end
+      if valid.include?(false)
+        return
+      else
         coordinates.each do |coordinate|
-          p @cells[coordinate]
           @cells[coordinate].place_ship(ship)
         end
       end
-    end
   end
 
-  def render
+  def render(display=false)
     x = (1..@width).to_a
-    y = ("A"..("A".ord+@length).chr).to_a
+    y = ("A"..("A".ord+(@length-1)).chr).to_a
     string = "  "
     x.each do |num|
       string << "#{num} "
     end
     y.each do |letter|
-      string << "\n#{letter}" + " ." * y.length
+      string << "\n#{letter}" + " ." * y.length + " "
     end
-    string << " \n"
-    puts string
+    string << "\n"
   end
 
 
   def valid_placement?(ship,coordinates)
+
     if ship.length == coordinates.length
+      length = ship.length - 1
       row = []
       column = []
       coordinates.each do |coordinate|
         letter = coordinate.split("").first
-        number = coordinate.split("").last
+        number = coordinate.split("").last.to_i
         row << number
         column << letter
-        if  ( (number..(number+ship.width).to_a == row && [letter]*ship.length == column ) || ( [number]*ship.width == row && (letter..((letter.ord)+ship.length).chr)).to_a == column )
-          true
-        end
       end
-    else
-      false
+      letter = coordinates[0].split("").first
+      number = coordinates[0].split("").last.to_i
+      limit = number + length
+      last = (letter.ord + length).chr
+        if (  ((number..limit).to_a == row) && ([letter]*length == column)  )  || (   (([number]*length) == row)    &&    ((letter..last).to_a == column)   )
+          true
+        else
+          false
+        end
+
     end
   end
 
--- test/board_test.rb
+++ test/board_test.rb
@@ -8,40 +8,40 @@ require './lib/board'
 class BoardTest < MiniTest::Test
 
   def setup
-    @cell_A1 = Cell.new("A1")
-    @cell_A2 = Cell.new("A2")
-    @cell_A3 = Cell.new("A3")
-    @cell_A4 = Cell.new("A4")
-    @cell_B1 = Cell.new("B1")
-    @cell_B2 = Cell.new("B2")
-    @cell_B3 = Cell.new("B3")
-    @cell_B4 = Cell.new("B4")
-    @cell_C1 = Cell.new("C1")
-    @cell_C2 = Cell.new("C2")
-    @cell_C3 = Cell.new("C3")
-    @cell_C4 = Cell.new("C4")
-    @cell_D1 = Cell.new("D1")
-    @cell_D2 = Cell.new("D2")
-    @cell_D3 = Cell.new("D3")
-    @cell_D4 = Cell.new("D4")
-    @cells = {
-      "A1" => @cell_A1,
-      "A2" => @cell_A2,
-      "A3" => @cell_A3,
-      "A4" => @cell_A4,
-      "B1" => @cell_B1,
-      "B2" => @cell_B2,
-      "B3" => @cell_B3,
-      "B4" => @cell_B4,
-      "C1" => @cell_C1,
-      "C2" => @cell_C2,
-      "C3" => @cell_C3,
-      "C4" => @cell_C4,
-      "D1" => @cell_D1,
-      "D2" => @cell_D2,
-      "D3" => @cell_D3,
-      "D4" => @cell_D4
-    }
+    # @cell_A1 = Cell.new("A1")
+    # @cell_A2 = Cell.new("A2")
+    # @cell_A3 = Cell.new("A3")
+    # @cell_A4 = Cell.new("A4")
+    # @cell_B1 = Cell.new("B1")
+    # @cell_B2 = Cell.new("B2")
+    # @cell_B3 = Cell.new("B3")
+    # @cell_B4 = Cell.new("B4")
+    # @cell_C1 = Cell.new("C1")
+    # @cell_C2 = Cell.new("C2")
+    # @cell_C3 = Cell.new("C3")
+    # @cell_C4 = Cell.new("C4")
+    # @cell_D1 = Cell.new("D1")
+    # @cell_D2 = Cell.new("D2")
+    # @cell_D3 = Cell.new("D3")
+    # @cell_D4 = Cell.new("D4")
+    # @cells = {
+    #   "A1" => @cell_A1,
+    #   "A2" => @cell_A2,
+    #   "A3" => @cell_A3,
+    #   "A4" => @cell_A4,
+    #   "B1" => @cell_B1,
+    #   "B2" => @cell_B2,
+    #   "B3" => @cell_B3,
+    #   "B4" => @cell_B4,
+    #   "C1" => @cell_C1,
+    #   "C2" => @cell_C2,
+    #   "C3" => @cell_C3,
+    #   "C4" => @cell_C4,
+    #   "D1" => @cell_D1,
+    #   "D2" => @cell_D2,
+    #   "D3" => @cell_D3,
+    #   "D4" => @cell_D4
+    # }
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
@@ -55,10 +55,10 @@ class BoardTest < MiniTest::Test
     assert_instance_of Hash, @board.cells
     @board.create_board(4, 4)
     refute_nil @board.cells
-    assert_equal "A1", @cells.key(@cell_A1)
+    assert_equal "A1", @board.cells.key(@cell_A1)
     assert_instance_of Cell, @board.cells["A1"]
     assert_equal 16, @board.length * @board.width
-    @cells.each do |key,value|
+    @board.cells.each do |key,value|
       assert_equal true, value == @board.cells[key]
     end
 
@@ -75,22 +75,25 @@ class BoardTest < MiniTest::Test
   end
 
   def test_valid_placement?
-    assert_equal true, @board.place(@cruiser,["A1","A2","A3"])
+    @board.create_board(4, 4)
+    @board.place(@cruiser,["A1","A2","A3"])
     assert_equal false, @board.valid_placement?(@submarine, ["A1", "B1"])
   end
 
   def test_place
+    @board.create_board(4, 4)
     @board.place(@cruiser,["A1","A2","A3"])
-    assert_equal @cruiser, @board.cells("A1").ship
-    assert_equal @cruiser, @board.cells("A2").ship
-    assert_equal @cruiser, @board.cells("A3").ship
+    assert_equal @cruiser, @board.cells["A1"].ship
+    assert_equal @cruiser, @board.cells["A2"].ship
+    assert_equal @cruiser, @board.cells["A3"].ship
     @board.place(@submarine,["A1","A2"])
-    assert_equal @cruiser, @board.cells("A1").ship
-    assert_equal @cruiser, @board.cells("A2").ship
-    assert_equal @cruiser, @board.cells("A3").ship
+    assert_equal @cruiser, @board.cells["A1"].ship
+    assert_equal @cruiser, @board.cells["A2"].ship
+    assert_equal @cruiser, @board.cells["A3"].ship
   end
 
   def test_render
+    @board.create_board(4, 4)
     @board.place(@cruiser,["A1","A2","A3"])
     assert_equal "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", @board.render
     assert_equal "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n", @board.render(true)
