a080f39361a08707b7659b5b5fa058173cb11b07
Mary Lang <mcl86@cornell.edu>
2019-08-23 11:15:05 -0600

    Add changes to be able to merge

--- test/player_test.rb
+++ test/player_test.rb
@@ -8,13 +8,16 @@ require './lib/player'
 class PlayerTest < MiniTest::Test
 
   def setup
-    @player = Player.new
-    @computer = Player.new
+    @player = Player.new("Mario",true)
+    @computer = Player.new("computer",false)
   end
 
   def test_it_exists
     assert_instance_of Player, @player
     assert_instance_of Player, @computer
   end
+
+  def test_it_has_a_name
+    assert_equal "Mario",
 
 end
