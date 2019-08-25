739d40646f9ef4ebc0ca4dbc4d8906fb0ee76109
Mary Lang <mcl86@cornell.edu>
2019-08-25 12:42:44 -0600

    Update game tests and add runner file
    Co-authored-by: Michael Cooper <8922930+CoopTang@users.noreply.github.com>

--- test/game_test.rb
+++ test/game_test.rb
@@ -71,129 +71,60 @@ class GameTest < MiniTest::Test
   end
 
   def test_a_turn
-    new_turn = Turn.new("B2", @game_data[:player], @game[:computer])
-    assert_equal new_turn, @game.turns.last
+    turn1 = Turn.new("B2", @game_data[:player], @game[:computer])
+    assert_equal turn1, @game.turns.last
     assert_equal true, @c_board.cells["B2"].fired_upon?
   end
 
   def test_feedback_for_miss
-    new_turn = Turn.new("B2", @game_data[:player], @game[:computer])
+    turn1 = Turn.new("B2", @game_data[:player], @game[:computer])
     assert_equal "Your shot on B2 was a miss.", @game.turn_feedback
-    new_turn2 = Turn.new("C3", @game_data[:computer, @game[:player])
+    turn2 = Turn.new("C3", @game_data[:computer, @game[:player])
     assert_equal "My shot on C3 was a miss.", @game.turn_feedback
   end
 
   def test_feedback_for_hit
     @p_board.place(@p_cruiser, ["A1", "A2", "A3"])
-    new_turn1 = Turn.new("A1", @game_data[:computer, @game[:player])
-    assert_equal "My shot on A1 was a hit!", @game.turn_feedback
+    turn1 = Turn.new("A1", @game_data[:computer, @game[:player])
+    assert_equal "My shot on A1 was a hit!", @game.feedback
   end
 
   def test_feedback_for_sunk
     @p_board.place(@p_cruiser, ["A1", "A2", "A3"])
-    new_turn1 = Turn.new("A1", @game_data[:computer, @game[:player])
-    new_turn2 = Turn.new("A2", @game_data[:computer, @game[:player])
-    new_turn3 = Turn.new("A3", @game_data[:computer, @game[:player])
-    assert_equal "My shot on A3 sunk the ship!", @game.turn_feedback
+    turn1 = Turn.new("A1", @game_data[:computer, @game[:player])
+    turn2 = Turn.new("A2", @game_data[:computer, @game[:player])
+    turn3 = Turn.new("A3", @game_data[:computer, @game[:player])
+    assert_equal "My shot on A3 sunk the ship!", @game.feedback(turn_3)
+  end
+
+  def test_turn_feedback
+    @p_board.place(@p_cruiser, ["A1", "A2", "A3"])
+    turn1 = Turn.new("A1", @game_data[:computer, @game[:player])
+    turn2 = Turn.new("D3", @game_data[:player], @game_data[:computer])
+    assert_equal "My shot on A1 was a hit!\nYour shot on D3 was a miss.", @game.feedback(turn1,turn2)
   end
 
 
-  def test_game_ends_when_a_ship_is_sunk
+
+  def test_game_over?
     @p_board.place(@p_cruiser, ["A1", "A2", "A3"])
-    new_turn1 = Turn.new("A1", @game_data[:computer, @game[:player])
-    new_turn2 = Turn.new("A2", @game_data[:computer, @game[:player])
-    new_turn3 = Turn.new("A3", @game_data[:computer, @game[:player])
-    any_ships_sunk = @game[:player][:ships].any? do |ship|
-      ship.sunk?
-    end
-    assert_equal true, @game.check_for_sunk_ships
+    turn1 = Turn.new("A1", @game_data[:computer], @game_data[:player])
+    turn2 = Turn.new("A2", @game_data[:computer], @game_data[:player])
+    turn3 = Turn.new("A3", @game_data[:computer], @game_data[:player])
+    @p_board.place(@p_submarine, ["C2", "D2"])
+    assert_equal false, @game.game_over?(@game_data[:player])
+    turn4 = Turn.new("C2", @game_data[:computer], @game_data[:player])
+    turn5 = Turn.new("D2", @game_data[:computer], @game_data[:player])
+    assert_equal true, @game.game_over?(@game_data[:player])
+  end
+
+  def test_end_game_feedback
+    @p_board.place(@p_cruiser, ["A1", "A2", "A3"])
+    turn1 = Turn.new("A1", @game_data[:computer], @game_data[:player])
+    turn2 = Turn.new("A2", @game_data[:computer], @game_data[:player])
+    turn3 = Turn.new("A3", @game_data[:computer], @game_data[:player])
+    @p_board.place(@p_submarine, ["C2", "D2"])
+    turn4 = Turn.new("C2", @game_data[:computer], @game_data[:player])
+    turn5 = Turn.new("D2", @game_data[:computer], @game_data[:player])
     assert_equal "I won!", @game.winner_feedback
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  def test_coordinate_is_valid
-  skip
-    @turn.get_coordinate
-    assert_equal true, @turn.valid_fire?
-  end
-
-  def test_feedback_to_user_invalid_coordinate
-  skip
-    @turn.prompt_for_user_shot
-    assert_equal "Please enter a valid coordinate", @turn.user_fires
-  end
-
-  def test_valid_fire?
-  skip
-    @turn.prompt_for_user_shot
-    assert_equal true, @turn.valid_fire?
-  end
-
-  def test_it_displays_user_results_for_miss
-  skip
-    #must test using input on known empty space
-    @turn.prompt_for_user_shot
-    @turn.user_fires
-    assert_equal "Your shot on #{@shot} was a miss.", @turn.user_result
-  end
-
-  def test_it_displays_user_results_for_hit
-    #must test using input on known ship
-    @turn.prompt_for_user_shot
-    @turn.user_fires
-    assert_equal "Your shot on #{@shot} was a hit!", @turn.user_result
-  end
-
-  def test_it_displays_user_results_for_sink
-    #must test using input on known ship with 1 hit left
-    @turn.prompt_for_user_shot
-    @turn.user_fires
-    assert_equal "Your shot on #{@shot} sunk the ship!", @turn.user_result
-  end
-
-  def test_it_displays_all_results
-    @turn.prompt_for_user_shot
-    @turn.user_fires
-    @computer_turn.computer_fire
-    assert_equal  "" + "\n" + "" , @turn.display_results
-  end
-
-end
